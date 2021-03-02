2e264fc70a6a3d2146fb6f4502d61cee380aaeeb
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-03-01 19:47:44 -0500

    commit tree e9f493134245ff9d83e46c919b33a3b71a14eaaf

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes'
 
 class Quotes extends Component {
 
+  constructor(){
+    super()
+    this.state = {
+      quotes: []
+    }
+  }
+  
+  
   render() {
     return (
       <div>
@@ -15,11 +24,14 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              {this.props.quotes.map(quote => 
+                <QuoteCard 
+                  quote={quote} 
+                  upVote={this.props.upvoteQuote} 
+                  downVote={this.props.downvoteQuote} 
+                  removeQuote={this.props.removeQuote}
+                />)
+              }
             </div>
           </div>
         </div>
@@ -28,5 +40,19 @@ class Quotes extends Component {
   }
 }
 
+const mapStateToProps = state => {
+    return {
+      quotes: state.quotes
+    }
+}
+
+const mapDispatchToProps = dispatch =>{
+  return {
+    upvoteQuote: (quoteId) => dispatch(upvoteQuote(quoteId)),
+    downvoteQuote: (quoteId) => dispatch(downvoteQuote(quoteId)),
+    removeQuote: (quoteId) => dispatch(removeQuote(quoteId))
+  }
+}
+
 //add arguments to connect as needed
-export default connect()(Quotes);
+export default connect(mapStateToProps, mapDispatchToProps)(Quotes);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,31 @@
+import uuid from 'uuid';
+
 export default (state = [], action) => {
-  return state;
+
+  let idx;
+  
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote];
+
+    case 'REMOVE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.quoteId)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)]
+
+    case 'UPVOTE_QUOTE':
+      let upvoteQuote = state.find(quote => quote.id === action.quoteId)
+      let newUpQuote = [{...upvoteQuote, votes: upvoteQuote.votes + 1}]
+      return newUpQuote
+
+    case 'DOWNVOTE_QUOTE':
+      let downvoteQuote = state.find(quote => quote.id === action.quoteId)
+      if(downvoteQuote.votes > 0) {
+        let newDownQuote = [{...downvoteQuote, votes: downvoteQuote.votes - 1}]
+        return newDownQuote
+      }
+      return [downvoteQuote]
+
+    default:
+      return state;
+  }
 }
